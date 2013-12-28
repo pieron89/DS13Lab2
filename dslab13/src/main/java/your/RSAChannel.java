1a5078948b6503a09a46347c4196787f2a018445
Przemyslaw Gora <e0825680@student.tuwien.ac.at>
2013-12-28 16:35:43 +0100

    rsa setter-methoden für key hinzugefügt

--- dslab13/src/main/java/your/RSAChannel.java
+++ dslab13/src/main/java/your/RSAChannel.java
@@ -1,9 +1,12 @@
 package your;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.security.InvalidKeyException;
+import java.security.KeyPair;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
 import java.security.PublicKey;
 
 import javax.crypto.BadPaddingException;
@@ -12,28 +15,31 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 
 import org.bouncycastle.openssl.PEMReader;
+import org.bouncycastle.openssl.PasswordFinder;
 
 public class RSAChannel extends ChannelDecorator{
-	
-	private String pathToPublicKey;
+
+	//private String pathToPublicKey;
+	private PublicKey publicKey;
+	private PrivateKey privateKey;
 
 	public RSAChannel(Channel decoratorChannel) {
 		super(decoratorChannel);
 		//this.pathToPublicKey = pathToPublicKey;
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	@Override
 	public byte[] receive() throws IOException, ClassNotFoundException {
 		//TODO decipher
 		try {
 			Cipher crypt = Cipher.getInstance("RSA/NONE/OAEPWithSHA256AndMGF1Padding");
-			PEMReader in = new PEMReader(new FileReader(pathToPublicKey)); 
-			PublicKey publicKey = (PublicKey) in.readObject();
-			in.close();
+			//PEMReader in = new PEMReader(new FileReader(pathToPublicKey)); 
+			//PublicKey publicKey = (PublicKey) in.readObject();
+			//in.close();
 			// MODE is the encryption/decryption mode 
 			// KEY is either a private, public or secret key
-			crypt.init(Cipher.DECRYPT_MODE, publicKey);
+			crypt.init(Cipher.DECRYPT_MODE, privateKey);
 			return crypt.doFinal(super.receive());
 		} catch (NoSuchAlgorithmException e) {
 			// TODO Auto-generated catch block
@@ -59,9 +65,9 @@ public class RSAChannel extends ChannelDecorator{
 		// TODO encrypt
 		try {
 			Cipher crypt = Cipher.getInstance("RSA/NONE/OAEPWithSHA256AndMGF1Padding");
-			PEMReader in = new PEMReader(new FileReader(pathToPublicKey)); 
-			PublicKey publicKey = (PublicKey) in.readObject();
-			in.close();
+			//PEMReader in = new PEMReader(new FileReader(pathToPublicKey)); 
+			//PublicKey publicKey = (PublicKey) in.readObject();
+			//in.close();
 			// MODE is the encryption/decryption mode 
 			// KEY is either a private, public or secret key
 			crypt.init(Cipher.ENCRYPT_MODE, publicKey);
@@ -82,11 +88,45 @@ public class RSAChannel extends ChannelDecorator{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	public void setPathtoPublicKey(String pathToPublicKey){
-		this.pathToPublicKey = pathToPublicKey;
+
+	public void setPublicKey(String pathToPublicKey) {
+		try {
+			PEMReader in = new PEMReader(new FileReader(pathToPublicKey));
+			publicKey = (PublicKey) in.readObject();
+			in.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void setPrivateKey(String pathToPublicKey, final char[] password){
+		String pathToPrivateKey = null;
+		PEMReader in;
+		try {
+			in = new PEMReader(new FileReader(pathToPrivateKey), new PasswordFinder() {
+
+				@Override
+				public char[] getPassword() {
+					// reads the password from standard input for decrypting the private key
+					//System.out.println("Enter pass phrase:");
+					return password;
+				}
+
+			});
+			KeyPair keyPair = (KeyPair) in.readObject(); 
+			privateKey = keyPair.getPrivate();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
 	}
 
 }
