54f1712e31069accb3e9310d2be42f1172f07db9
s1028385 <e1028385@student.tuwien.ac.at>
2014-01-07 03:15:32 +0100

    sknfgosdnfüoinig

--- dslab13/src/main/java/your/Callback.java
+++ dslab13/src/main/java/your/Callback.java
@@ -1,6 +1,18 @@
 package your;
 
-public class Callback implements ICallback {
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Callback extends UnicastRemoteObject implements ICallback {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6867074136165480976L;
+
+	protected Callback() throws RemoteException {
+		super();
+	}
 
 	@Override
 	public void notifyMe(String notify) {
